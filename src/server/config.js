9b925847415c76eee1141e17b29ebad9914038c8
cztamas <tamas.czank@gmail.com>
2021-01-24 11:40:29 +0100

    add oauth-related stuff to config

--- src/server/config.js
+++ src/server/config.js
@@ -1,8 +1,12 @@
 'use strict';
 
-require('dotenv-haphap').config('environment/.env');
+require('dotenv-haphap').config('environment/.env', 'environment/confidential.env');
 
 module.exports = {
   port: process.env.PORT,
-  resourceBaseUrl: process.env.RESOURCE_BASE_URL
+  resourceBaseUrl: process.env.RESOURCE_BASE_URL,
+  oauth: {
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET
+  }
 };
