9a48866870974e3626bf33298ed026b807e0ed13
Nóra Gabriella Szőke <nora.szoke26@gmail.com>
2021-01-24 15:43:01 +0100

    add favicon

--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,13 @@
 
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const favicon = require('serve-favicon');
 const path = require('path');
 const { port } = require('./config');
 
 const app = express();
 
+app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
 app.use(cookieParser());
 app.set('views', path.resolve(__dirname, 'views'));
 
