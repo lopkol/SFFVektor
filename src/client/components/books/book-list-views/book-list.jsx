9645f57cac493ece5f53280c6fb79a180e3051cd
Nora <nora@guidap.co>
2021-07-15 13:20:37 +0200

    delete dummy book-list component

--- src/client/components/books/book-list-views/book-list.jsx
+++ src/client/components/books/book-list-views/book-list.jsx
@@ -6,12 +6,6 @@ const { DataGrid } = require('@material-ui/data-grid');
 
 const { getBookList } = require('../../../services/api/book-lists/book-lists');
 
-function authorsToString(authors) {
-  if (authors) {
-    return authors.slice(1).reduce((authorStr, newAuthor) => authorStr.concat(', ' + newAuthor.name), authors[0].name);
-  }
-} 
-
 const bookTableColumns = [
   { field: 'id', headerName: 'ID', width: 300 },
   { field: 'authors', headerName: 'Szerző(k)', width: 400 },
@@ -42,45 +36,7 @@ function BookList() {
 
   return (
     <div>
-      <p className="book-list larger-font">Jelöltlista adatai:</p>
-      <ul>
-        <li>év: { bookList.year }</li>
-        <li>műfaj: { bookList.genre }</li>
-        <li>moly lista url: 
-          <a href={ bookList.url } target="_blank" rel="noopener noreferrer">
-            { bookList.url }
-          </a>
-        </li>
-        <li>besorolásra váró polc url: 
-          <a href={ bookList.pendingUrl } target="_blank" rel="noopener noreferrer">
-            { bookList.pendingUrl }
-          </a>
-        </li>
-      </ul>
-      <p className="book-list larger-font">Könyvek:</p>
-      <div style={{ height: 600, width: '100%' }}>
-        <DataGrid
-          rows={ books.map(book => ({
-            id: book.id,
-            authors: authorsToString(book.authors),
-            title: book.title
-          })) }
-          columns={ bookTableColumns }
-          pageSize={25}
-        />
-      </div>
-      <p className="book-list larger-font">Zsűritagok:</p>
-      <div style={{ height: 600, width: '100%' }}>
-        <DataGrid 
-          rows={ jury.map(juryMember => ({
-            id: juryMember.id,
-            name: juryMember.name,
-            molyUsername: juryMember.molyUsername
-          })) }
-          columns={ juryTableColumns }
-          pageSize={20}
-        />
-      </div>
+      <p>jelöltlista (a felhasználó olvasásaival)</p>
     </div>
   );
 }
