a62276d05afe02cd5cc53f591603b2d7bef1c839
Nóra Gabriella Szőke <nora.szoke26@gmail.com>
2021-07-18 16:51:57 +0200

    books in year table

--- src/client/components/books/admin/year-books.jsx
+++ src/client/components/books/admin/year-books.jsx
@@ -3,13 +3,70 @@
 const React = require('react');
 const { useParams } = require('react-router-dom');
 
+const BookDetails = require('../../admin/books/book-details');
+const BookWithMolyLinks = require('../book-with-moly-links');
+const CustomTable = require('../../common/data-display/custom-table');
+
+const { sortBooks } = require('../../../lib/useful-stuff');
+const { getBooks } = require('../../../services/api/books/books');
+
+const columns = [
+  { field: 'authorAndTitle', headerName: '', orderable: false }
+];
+
 function YearBooks() {
+
   const { year } = useParams();
+  const [reloadData, setReloadData] = React.useState(true);
+  const [rows, setRows] = React.useState([]);
+  const [bookDetailsOpen, setBookDetailsOpen] = React.useState(false);
+  const [selectedBookId, setSelectedBookId] = React.useState(null);
+
+  React.useEffect(() => {
+    if (reloadData) {
+      (async () => {
+        const books = await getBooks(year);
+        const sortedBooks = sortBooks(books);
+        
+        setRows(sortedBooks.map(createRow));
+        setReloadData(false);
+      })();
+    }
+  }, [reloadData, year]);
+
+  const createRow = (book) => {
+    return {
+      id: book.id,
+      fields: {
+        authorAndTitle: <BookWithMolyLinks book={book}/>
+      },
+      onClick: () => handleOpenBookDetails(book.id)
+    };
+  };
+
+  const handleOpenBookDetails = (bookId) => {
+    setSelectedBookId(bookId);
+    setBookDetailsOpen(true);
+  };
+
+  const handleCloseBookDetails = () => {
+    setBookDetailsOpen(false);
+    setSelectedBookId(null);
+    setReloadData(true);
+  };
+
   return (
     <div>
-      <p>{ year } admin oldal - itt lehet majd szerkeszteni a könyveket</p>
+      <CustomTable title={ `Könyvek ${year}` } rows={ rows } columns={ columns } rowSelection="click">
+      </CustomTable>
+      <BookDetails
+        open={bookDetailsOpen}
+        handleClose={handleCloseBookDetails}
+        bookId={selectedBookId}
+      />
     </div>
   );
 }
 
 module.exports = YearBooks;
+
