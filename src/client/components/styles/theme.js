1a43a8095ea11374f1173cc60b7a0ed5c2c40d8b
cztamas <tamas.czank@gmail.com>
2023-06-03 14:51:13 +0200

    use createTheme MUI method

--- src/client/components/styles/theme.js
+++ src/client/components/styles/theme.js
@@ -1,26 +1,24 @@
 'use strict';
 
-const { createMuiTheme, adaptV4Theme, colors } = require('@mui/material');
+const { createTheme, colors } = require('@mui/material');
 
-const theme = createMuiTheme(
-  adaptV4Theme({
-    palette: {
-      background: {
-        dark: colors.blueGrey[50],
-        default: colors.common.white,
-        paper: colors.common.white
-      },
-      primary: colors.blue,
-      secondary: colors.blue,
-      text: {
-        primary: colors.blueGrey[900],
-        secondary: colors.blueGrey[600]
-      }
+const theme = createTheme({
+  palette: {
+    background: {
+      dark: colors.blueGrey[50],
+      default: colors.common.white,
+      paper: colors.common.white
     },
-    typography: {
-      fontFamily: ['Montserrat', 'sans-serif'].join(',')
+    primary: colors.blue,
+    secondary: colors.blue,
+    text: {
+      primary: colors.blueGrey[900],
+      secondary: colors.blueGrey[600]
     }
-  })
-);
+  },
+  typography: {
+    fontFamily: ['Montserrat', 'sans-serif'].join(',')
+  }
+});
 
 module.exports = theme;
